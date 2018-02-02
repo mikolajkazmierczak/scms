2dc960a39deb77ca04914586b0f96a7386791c43
mikolajkazmierczak <mikolaj@kazmierczak.pl>
2018-02-02 09:38:06 +0100

    Little fix (inf)
    Title

--- scms/editing_area_elements/add_button.php
+++ scms/editing_area_elements/add_button.php
@@ -1,20 +1,20 @@
 <div class="o-add">
-  <img class="o-add__img" src="http://localhost/scms/icons/add.svg" alt="">
+  <img class="o-add__img" src="http://localhost/scms/icons/black/add.svg" alt="">
   <h1 class="o-add__title">Dodaj</h1>
 
   <div class="o-add__menu">
     <div class="o-add__item">
-      <img class="o-add__item-img" src="http://localhost/scms/icons/text.svg" alt="">
+      <img class="o-add__item-img" src="http://localhost/scms/icons/black/text.svg" alt="">
       <h1 class="o-add__item-title">Tekst</h1>
     </div>
 
     <div class="o-add__item">
-      <img class="o-add__item-img" src="http://localhost/scms/icons/img.svg" alt="">
+      <img class="o-add__item-img" src="http://localhost/scms/icons/black/img.svg" alt="">
       <h1 class="o-add__item-title">Zdjęcie</h1>
     </div>
 
     <div class="o-add__item">
-      <img class="o-add__item-img" src="http://localhost/scms/icons/section.svg" alt="">
+      <img class="o-add__item-img" src="http://localhost/scms/icons/black/section.svg" alt="">
       <h1 class="o-add__item-title">Sekcja</h1>
     </div>
   </div>
